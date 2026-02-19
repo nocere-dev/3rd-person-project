1ead35ae69475780f85bacafdaa12d0b9820fd3f
Mountain King <odj.mountain@gmail.com>
2026-02-19 00:59:13 +0000

    improved toolbelt

--- Assets/Ollie-test-stuff/toolblet.cs
+++ Assets/Ollie-test-stuff/toolblet.cs
@@ -9,9 +9,6 @@ public class toolblet : MonoBehaviour
     public Tools[] assassin_belt;
 
     [SerializeField] private int selectedToolIndex = 0;
-
-    //public float throwSpeed = 10f;
-    
     
     void Start()
     {
@@ -21,14 +18,11 @@ public class toolblet : MonoBehaviour
     
     void Update()
     {
-        for (int i = 0; i < assassin_belt.Length; i++)
+        if (Input.GetMouseButtonDown(1))
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
-            {
-                selectedToolIndex = i;
-                Debug.Log("Selected tool: " + assassin_belt[i].name);
-            }
-        }        
+            selectedToolIndex = (selectedToolIndex + 1) % assassin_belt.Length;
+            Debug.Log("Selected tool: " + assassin_belt[selectedToolIndex].name);
+        }       
     }
 
     void FixedUpdate()
@@ -41,43 +35,25 @@ public class toolblet : MonoBehaviour
 
     void Throw(int toolIndex)
     {
-        
-        Ray aimRay = new Ray(throwPoint.position, throwPoint.forward);
-        Vector3 throwDirection = aimRay.direction;
-
-        
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
         RaycastHit hit;
-        bool hasHit = Physics.Raycast(aimRay, out hit, 100f);
 
-        
+        bool hasHit = Physics.Raycast(ray, out hit, 100f);
+
+        Vector3 spawnPosition = throwPoint.position;
+        Vector3 throwDirection = ray.direction;
+
         if (hasHit)
         {
             throwDirection = (hit.point - throwPoint.position).normalized;
         }
 
-        GameObject gear = Instantiate(assassin_belt[toolIndex].toolPrefab, throwPoint.position, Quaternion.identity);
-
-        // private Vector3 velocity;
-        // private Vector3 gravity = Physics.gravity;
-
-        // velocity = throwDirection * throwSpeed;
-
-        // velocity += gravity * Time.deltaTime;
-
-        // transform.position += velocity * Time.deltaTime;
-
-        // if(velocity.magnitude > 0.1f)
-        //  {
-        //     transform.rotation = Quaternion.LookRotation(velocity);
-        //  }
-
-        Rigidbody toolRb = gear.GetComponent<Rigidbody>();
+        GameObject thrownTool = Instantiate(assassin_belt[toolIndex].toolPrefab, spawnPosition, Quaternion.identity);
+        Rigidbody toolRb = thrownTool.GetComponent<Rigidbody>();
 
         if (toolRb != null)
         {
             toolRb.linearVelocity = throwDirection * assassin_belt[toolIndex].throwSpeed;
         }
-
-        Debug.Log("Threw tool: " + assassin_belt[toolIndex].name);
     }
 }
