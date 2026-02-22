6d054807f8753cb31a4fcfc35ffeb35ba194deee
Alekss <agurjanovs09@gmail.com>
2026-02-22 14:43:06 +0000

    something?

--- Assets/Scripts/environmental/DoorButton.cs
+++ Assets/Scripts/environmental/DoorButton.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 
-public class DoorButton : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+public GameObject promptUI;
 
-    // Update is called once per frame
-    void Update()
+private void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.CompareTag("Player"))
     {
-        
+        playerInRange = true;
+        promptUI.SetActive(true);
+    }
+}
+
+public void OnTriggerExit2D(Collider2D other)
+{
+    if (other.CompareTag("Player"))
+    {
+        playerInRange = false;
+        promptUI.SetActive(false);
     }
 }
