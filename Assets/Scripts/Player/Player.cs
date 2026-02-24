3d4c32a45d3341dbc5de67f56302081fb3c45745
sofewhours <lukerob39@outlook.com>
2026-02-24 00:22:23 +0000

    new player inputs update, removed old movement script and ladders are half implemented

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,31 +1,74 @@
 using System;
-using System.Numerics;
 using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.TextCore.Text;
 
 public class Player : MonoBehaviour
 {
     public Transform cam;
 
+    [Header("Player Settings")]
     [SerializeField] private float mouseSensitivity = 1f;
     [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private float jumpSpeed = 5f;
+    [SerializeField] private float climbingSpeed = 2f;
     [SerializeField] private float mass = 1f;
-    [SerializeField] private PlayerInputs input;
+    [SerializeField] private float acceleration = 10f;
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private float turnSmoothVelocity;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    public float Height
+    {
+        get => controller.height;
+        set => controller.height = value;
+    }
+
+    public event Action OnBeforeMove;
+
+    internal float movementSpeedMultiplier;
+
+    State _state;
+
+    public State CurrentState
+    {
+        get => _state;
+        set
+        {
+            _state = value;
+            velocity = Vector3.zero;
+        }
+    }
+
+    public enum State
+    {
+        Walking,
+        Climbing
+    }
+
+    // Inputs
+    PlayerInput input;
+    InputAction moveAction;
+    InputAction lookAction;
+    InputAction jumpAction;
+    InputAction crouchAction;
 
     CharacterController controller;
 
-    Vector2 look;
-    Vector3 velocity;
+    private Vector2 look;
+    internal Vector3 velocity;
+    private Vector3 currentVelocity;
 
     void Awake()
     {
         controller = GetComponent<CharacterController>();
-        if (!input) input = GetComponent<PlayerInputs>();
+        if (!input) input = GetComponent<PlayerInput>();
+        moveAction = input.actions["Move"];
+        lookAction = input.actions["Look"];
+        jumpAction = input.actions["Jump"];
+        crouchAction = input.actions["Crouch"];
     }
     void Start()
     {
@@ -35,9 +78,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateGravity();
-        UpdateLook();
-        UpdateMovement();
+        movementSpeedMultiplier = 1f;
+        switch (CurrentState)
+        {
+            case State.Walking:
+                UpdateGravity();
+                UpdateMovement();
+                UpdateLook();
+                break;
+            case State.Climbing:
+                UpdateClimbing();
+                UpdateLook();
+                break;
+        }
     }
 
     void UpdateGravity()
@@ -48,10 +101,13 @@ public class Player : MonoBehaviour
 
     void UpdateMovement()
     {
-        // var x = Input.GetAxisRaw("Horizontal");
-        // var y = Input.GetAxisRaw("Vertical");
-        var moveInput = input.Move();
+        movementSpeedMultiplier = 1f;
+        OnBeforeMove?.Invoke();
+
+        var moveInput = moveAction.ReadValue<Vector2>();
         Vector3 input = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+
+        input *= movementSpeed * movementSpeedMultiplier;
 
         if (input.magnitude > 0.1f)
         {
@@ -61,18 +117,32 @@ public class Player : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             
-            if (input.JumpPressed && controller.isGrounded){
+            if (jumpAction.WasPressedThisFrame() && controller.isGrounded){
                 velocity.y += jumpSpeed;
             }
-            controller.Move((moveDir * movementSpeed + velocity) * Time.deltaTime);
+
+            Vector3 targetVelocity = moveDir * movementSpeed;
+            currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, acceleration * Time.deltaTime);
+            controller.Move((currentVelocity + velocity) * Time.deltaTime);
         }
-        input.ConsumeOneFrameButtons();
+    }
+
+    void UpdateClimbing()
+    {
+        var moveInput = moveAction.ReadValue<Vector2>();
+        Vector3 input = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+        input *= climbingSpeed;
+
+        var factor = acceleration * Time.deltaTime;
+        velocity = Vector3.Lerp(velocity, input, factor);
+
+        controller.Move(velocity * Time.deltaTime);
     }
 
     void UpdateLook()
     {
-        look.x += input.Look.x * mouseSensitivity;
-        look.y += input.Look.y * mouseSensitivity;
+        look.x += lookAction.ReadValue<Vector2>().x * mouseSensitivity;
+        look.y += lookAction.ReadValue<Vector2>().y * mouseSensitivity;
 
         look.y = Mathf.Clamp(look.y, -90f, 90f);
     }
