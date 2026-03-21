ddfacac05491b0b2fce8293083e0d640522f72dc
Nelson0407 <okenelson0907@gmail.com>
2026-03-21 14:49:55 +0000

    run added but jump cooked for some reason

--- Assets/Scripts/Player/PlayerSprinting.cs
+++ Assets/Scripts/Player/PlayerSprinting.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Player))]
 public class PlayerSprinting : MonoBehaviour {
     [SerializeField] private float sprintMultiplier = 1.5f;
+    private CharacterController controller;
+    Animator animator;
 
     private Player player;
     private PlayerInput playerInput;
@@ -13,10 +15,13 @@ public class PlayerSprinting : MonoBehaviour {
         player = GetComponent<Player>();
         playerInput = GetComponent<PlayerInput>();
         sprintAction = playerInput.actions["Sprint"];
+        animator = GetComponent<Animator>();
+        if (!animator) animator = GetComponentInChildren<Animator>();
     }
 
     private void OnEnable() {
         player.OnBeforeMove += OnBeforeMove;
+        
     }
 
     private void OnDisable() {
@@ -25,12 +30,20 @@ public class PlayerSprinting : MonoBehaviour {
 
     private void OnBeforeMove() {
         var sprintInput = sprintAction.ReadValue<float>();
-        if (sprintInput == 0) return;
+
+        if (sprintInput == 0)
+        {
+            animator.SetBool("isRunning", false);
+            return;
+        }
 
         var forwardMovementFactor =
             Mathf.Clamp01(Vector3.Dot(player.transform.forward, player.CurrentVelocity.normalized));
         var multiplier = Mathf.Lerp(1f, sprintMultiplier, forwardMovementFactor);
 
         player.movementSpeedMultiplier *= multiplier;
+
+        // Set animation
+        animator.SetBool("isRunning", true);
     }
 }
