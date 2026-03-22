72cc947215d005517b7d5894aa8ee7142d2f3198
sofewhours <lukerob39@outlook.com>
2026-03-22 19:50:24 +0000

    Revert "Update leve design (#47)"
    This reverts commit b0835fd596d96ae148aade95233a4a91314ec91a.

--- Assets/Scripts/environmental/DoorInteraction.cs
+++ Assets/Scripts/environmental/DoorInteraction.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
 
 public class DoorInteraction : MonoBehaviour {
-    [Header("Door & Box")]
-    public GameObject door;
-    public Animator boxAnimator; 
-    public string interactTrigger = "Interact";
+    [Header("Door & Box")] public GameObject door;
+    public Renderer boxRenderer;
+    public Color interactColor = Color.green;
 
-    [Header("UI")]
-    public GameObject pressEUI;
+    [Header("UI")] public GameObject pressEUI;
 
     private bool playerInRange;
 
@@ -17,8 +15,7 @@ public class DoorInteraction : MonoBehaviour {
     }
 
     private void Update() {
-        if (playerInRange && Input.GetKeyDown(KeyCode.E))
-            Interact();
+        if (playerInRange && Input.GetKeyDown(KeyCode.E)) Interact();
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -40,13 +37,14 @@ public class DoorInteraction : MonoBehaviour {
     }
 
     private void Interact() {
-        
-            boxAnimator.SetTrigger(interactTrigger);
+        if (boxRenderer != null)
+            boxRenderer.material.color = interactColor;
+
 
         if (door != null)
             door.SetActive(false);
 
-      
+
         if (pressEUI != null)
             pressEUI.SetActive(false);
 
