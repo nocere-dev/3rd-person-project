648a62e513ad263b3d7f60655b69aa967b22f8a7
Nelson0407 <okenelson0907@gmail.com>
2026-03-22 17:41:41 +0000

    Nelson branch again (#48)
    * //
    
    * run added but jump cooked for some reason
    
    * climb
    
    * murder is added
    
    * animations complete
    
    * fix jump and more
    
    ---------
    
    Co-authored-by: nocere <lukerob39@outlook.com>

--- Assets/Scripts/Player/PlayerKilling.cs
+++ Assets/Scripts/Player/PlayerKilling.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
 
 public class PlayerKilling : MonoBehaviour {
+    private CharacterController controller;
+    Animator animator;
+
     public float killRange;
 
     public LayerMask enemyMask;
@@ -10,6 +15,7 @@ public class PlayerKilling : MonoBehaviour {
     public GameObject indicator;
 
     private GameObject target;
+    private Player player;
 
     public PlayerToolbelt toolUses;
 
@@ -28,6 +34,21 @@ public class PlayerKilling : MonoBehaviour {
         Gizmos.DrawWireSphere(transform.position, killRange);
     }
 
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+        controller = GetComponent<CharacterController>();
+        if (!animator) animator = GetComponentInChildren<Animator>();
+    }
+
+    public void KillTarget()
+    {
+        if (target != null)
+        {
+            Destroy(target);
+            target = null;
+        }
+    }
     public void canAssassinate() {
         var colliders = Physics.OverlapSphere(transform.position, killRange, enemyMask);
 
@@ -35,6 +56,7 @@ public class PlayerKilling : MonoBehaviour {
             canKill = true;
             indicator.SetActive(true);
             target = colliders[0].gameObject;
+
         }
         else {
             canKill = false;
@@ -43,13 +65,30 @@ public class PlayerKilling : MonoBehaviour {
         }
     }
 
-    public void Assassinating() {
-        if (canKill && target != null) {
-            Destroy(target);
+    public void Assassinating()
+    {
+        if (canKill && target != null)
+        {
+            animator.SetTrigger("Attack");
 
-            toolUses.uses++;
-            canKill = false;
-            indicator.SetActive(false);
         }
     }
-}
+
+    IEnumerator DestroyAfterAnimation()
+    {
+        // Wait until animator actually switches to the Attack state
+        yield return null;
+
+        // Get current animation state info
+        AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+
+        // Wait for the full animation to finish
+        yield return new WaitForSeconds(state.length);
+
+        if (target != null)
+        {
+            Destroy(target);
+            target = null;
+        }
+    }
+    }
--- Assets/Scripts/Player/PlayerToolbelt.cs
+++ Assets/Scripts/Player/PlayerToolbelt.cs
@@ -15,13 +15,16 @@ public class PlayerToolbelt : MonoBehaviour {
     public TextMeshProUGUI usesIndicator;
 
     [SerializeField] private int selectedToolIndex;
+    private CharacterController controller;
+    Animator animator;
 
     private void Start() {
         toolNameText.text = assassin_belt[selectedToolIndex].name;
-        uses = 3;
+        animator = GetComponent<Animator>();
+        if (!animator) animator = GetComponentInChildren<Animator>();
     }
 
-
+    private void FixedUpdate() { }
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Q)) {
             selectedToolIndex = (selectedToolIndex + 1) % assassin_belt.Length;
@@ -29,43 +32,26 @@ public class PlayerToolbelt : MonoBehaviour {
             Debug.Log("Selected tool: " + assassin_belt[selectedToolIndex].name);
         }
 
-        if (Input.GetMouseButtonDown(0) && uses > 0)
-        {
-            Throw(selectedToolIndex);
-            if (assassin_belt[selectedToolIndex].name != "hand")
-            {
-                uses--;
-            }
-        }
-
-        usesIndicator.text = uses.ToString();
+        if (Input.GetMouseButtonDown(0)) Throw(selectedToolIndex);
+       
     }
+    public void SpawnTool()
+    {
+        var spawnPosition = throwPoint.position;
+        var throwDirection = Camera.main.transform.forward;
 
-    private void FixedUpdate() {
+        var thrownTool = Instantiate(assassin_belt[selectedToolIndex].toolPrefab, spawnPosition, Quaternion.identity);
+        var toolRb = thrownTool.GetComponent<Rigidbody>();
+
+        if (toolRb != null)
+            toolRb.linearVelocity = throwDirection * assassin_belt[selectedToolIndex].throwSpeed;
     }
-
     private void Throw(int toolIndex) {
+        animator.SetTrigger("Throw");
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 1, 0));
         RaycastHit hit;
 
         var hasHit = Physics.Raycast(ray, out hit, 100f, ~playerLayer);
 
-        var spawnPosition = throwPoint.position;
-        var throwDirection = ray.direction;
-
-        if (assassin_belt[toolIndex].toolPrefab != null)
-        {
-            var thrownTool = Instantiate(assassin_belt[toolIndex].toolPrefab, spawnPosition, Quaternion.identity);
-            var toolRb = thrownTool.GetComponent<Rigidbody>();
-            if (toolRb != null)
-            {
-                toolRb.linearVelocity = throwDirection * assassin_belt[toolIndex].throwSpeed;
-            }
-        }
-        
-        
-        
-
-        
     }
 }
