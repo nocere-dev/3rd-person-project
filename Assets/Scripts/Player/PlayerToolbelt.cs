c5e0312877e29fbdbf9ab5884ab5692debd590c6
Alekss <agurjanovs09@gmail.com>
2026-03-30 14:26:14 +0100

    Revert "Merge branch 'main' into UpdateLeveDesign"
    This reverts commit d3b56bf2a35fdd9a23bd7365ed7461e4a0b49c79, reversing
    changes made to 2f9c8c2e8cbbb1185bafc9e1ff8232d0fa00ac18.

--- Assets/Scripts/Player/PlayerToolbelt.cs
+++ Assets/Scripts/Player/PlayerToolbelt.cs
@@ -17,18 +17,15 @@ public class PlayerToolbelt : MonoBehaviour {
     [SerializeField] private int selectedToolIndex;
     private CharacterController controller;
     Animator animator;
-    private Vector3 _throwDirection;
 
     private void Start() {
         toolNameText.text = assassin_belt[selectedToolIndex].name;
         animator = GetComponent<Animator>();
         if (!animator) animator = GetComponentInChildren<Animator>();
-        usesIndicator.text = uses.ToString();
     }
 
     private void FixedUpdate() { }
-    private void Update() 
-    {
+    private void Update() {
         if (Input.GetKeyDown(KeyCode.Q)) {
             selectedToolIndex = (selectedToolIndex + 1) % assassin_belt.Length;
             toolNameText.text = assassin_belt[selectedToolIndex].name;
@@ -51,46 +48,10 @@ public class PlayerToolbelt : MonoBehaviour {
     }
     private void Throw(int toolIndex) {
         animator.SetTrigger("Throw");
-        if (Input.GetMouseButtonDown(0) && uses > 0)
-        {
-            if (assassin_belt[selectedToolIndex].name != "hand")
-            {
-                Throw();
-                uses--;
-                
-            }
-        }
-        usesIndicator.text = uses.ToString();
-    }
-    
-    public void SpawnTool()
-    {
-        if (assassin_belt[selectedToolIndex].toolPrefab)
-        {
-            var thrownTool = Instantiate(assassin_belt[selectedToolIndex].toolPrefab, throwPoint.position, Quaternion.LookRotation(_throwDirection));
-            var toolRb = thrownTool.GetComponent<Rigidbody>();
-            
-            if (toolRb != null)
-            {
-                toolRb.linearVelocity = _throwDirection * assassin_belt[selectedToolIndex].throwSpeed;
-            }
-        }
-    }
-    
-    private void Throw() 
-    {
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 1, 0));
         RaycastHit hit;
 
         var hasHit = Physics.Raycast(ray, out hit, 100f, ~playerLayer);
 
-        if (hasHit) {
-            _throwDirection = (hit.point - throwPoint.position).normalized;
-        }
-        else {
-            _throwDirection = ray.direction;
-        }
-        
-        animator.SetTrigger("Throw");
     }
 }
